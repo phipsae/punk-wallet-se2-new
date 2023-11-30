2116f0b20717867b19abfb3b2cc5ab4b596533e3
phipsae <philip.krause88@yahoo.de>
2023-11-29 19:46:01 -0500

    send function implemented

--- packages/nextjs/components/wallet/ERC20TokenTransaction.tsx
+++ packages/nextjs/components/wallet/ERC20TokenTransaction.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ERC20Input } from "../scaffold-eth/Input/ERC20Input";
 import { publicClientSelector } from "./publicClientSelector";
 import { walletClientSelector } from "./walletClientSelector";
-import { parseAbi } from "viem";
-import { AddressInput, IntegerInput } from "~~/components/scaffold-eth/Input";
+import { parseAbi, parseEther } from "viem";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { Spinner } from "~~/components/assets/Spinner";
+import { AddressInput } from "~~/components/scaffold-eth/Input";
+import { notification } from "~~/utils/scaffold-eth";
 
 interface ERC20TokenTransactionProps {
   account: any;
@@ -12,13 +16,16 @@ interface ERC20TokenTransactionProps {
 
 const abi = parseAbi([
   // "function balanceOf(address owner) view returns (uint256)",
-  // "event Transfer(address indexed from, address indexed to, uint256 amount)",
+  "event Transfer(address indexed from, address indexed to, uint256 amount)",
   "function transfer(address to, uint256 value) public returns (bool)",
 ]);
 
 export const ERC20TokenTransaction = ({ account, tokenAddress, selectedChain }: ERC20TokenTransactionProps) => {
   const [to, setTo] = useState("");
   const [amount, setAmount] = useState("");
+  const [isSent, setIsSent] = useState(false);
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const walletClient = walletClientSelector(selectedChain, account);
   const publicClient = publicClientSelector(selectedChain);
@@ -30,14 +37,40 @@ export const ERC20TokenTransaction = ({ account, tokenAddress, selectedChain }: 
         address: tokenAddress,
         abi: abi,
         functionName: "transfer",
-        args: [to, BigInt(amount)],
+        args: [to, parseEther(amount)],
       });
 
       const requestAny: any = response.request;
       console.log(requestAny);
       await walletClient.writeContract(requestAny);
+      setIsSent(true);
+      setIsLoading(true);
+
+      const unwatch = publicClient.watchContractEvent({
+        address: tokenAddress,
+        abi: abi,
+        eventName: "Transfer",
+        args: { from: account.address },
+        onLogs: logs => {
+          console.log(logs);
+          setIsConfirmed(true);
+          setIsLoading(false);
+        },
+      });
+      console.log(unwatch);
     }
   };
+
+  useEffect(() => {
+    if (isSent) {
+      notification.success("Transaction successfully submitted");
+      setIsSent(false);
+    }
+    if (isConfirmed && !isSent) {
+      notification.success("Transaction successfully confirmed");
+      setIsConfirmed(false);
+    }
+  }, [isSent, isConfirmed]);
 
   return (
     <>
@@ -45,15 +78,25 @@ export const ERC20TokenTransaction = ({ account, tokenAddress, selectedChain }: 
         <AddressInput value={to ?? ""} onChange={to => setTo(to)} placeholder="Address Receiver" />
       </span>
       <span className="w-1/2 mb-5">
-        <IntegerInput value={amount} onChange={amount => setAmount(amount.toString())} placeholder="#" />
+        <ERC20Input value={amount} onChange={amount => setAmount(amount)} placeholder="#" />
       </span>
+
       <button
         className="btn btn-primary h-[2.2rem] min-h-[2.2rem] mt-auto"
         onClick={() => {
           txRequest();
         }}
       >
-        Send
+        {!isConfirmed && isLoading ? (
+          <div className="flex w-[100px] justify-center">
+            <Spinner width="100" height="100"></Spinner>
+          </div>
+        ) : (
+          <div className="flex flex-row w-full">
+            <EnvelopeIcon className="h-4 w-4" />
+            <span className="mx-3"> Send </span>
+          </div>
+        )}
       </button>
     </>
   );
