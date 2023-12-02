b4c680bd7a6b8aba86a0694caf20a28ae6575042
phipsae <philip.krause88@yahoo.de>
2023-12-02 15:45:51 -0500

    implement delete modal

--- packages/nextjs/components/wallet/AccountSwitcher/ShowPrivateKey.tsx
+++ packages/nextjs/components/wallet/AccountSwitcher/ShowPrivateKey.tsx
@@ -1,58 +1,8 @@
-import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
-import { Address } from "~~/components/scaffold-eth";
-import { useSharedState } from "~~/sharedStateContext";
-
 interface ShowPrivateKeyProps {
   address: string;
 }
 
 export const ShowPrivateKey = ({ address }: ShowPrivateKeyProps) => {
-  const { accounts, setAccounts } = useSharedState();
-
-  const deleteAccount = (address: string) => {
-    const filteredAccounts = accounts.filter(acc => acc.account.address !== address);
-    setAccounts(filteredAccounts);
-  };
-  return (
-    <>
-      <dialog id="delete_account" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg flex flex-row gap-2 justify-center">
-            Delete Account <Address address={address} />?
-          </h3>
-          <div className="flex flex-row items-start gap-5 py-4">
-            <div>
-              <ExclamationCircleIcon className="h-20 w-20 text-start" />
-            </div>
-            <div>
-              <span className="font-bold">
-                Warning: If you delete your account there is no way to revert it. So make sure your private key is
-                securely saved.
-              </span>
-            </div>
-          </div>
-
-          <div className="modal-action">
-            <form method="dialog">
-              <div className="flex flex-row gap-5">
-                <div className="w-full">
-                  <button
-                    className="btn btn-error"
-                    onClick={() => {
-                      deleteAccount(address);
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-                <div>
-                  <button className="btn"> Close </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </dialog>
-    </>
-  );
+  console.log(address);
+  return <></>;
 };
